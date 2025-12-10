c11a3bf06cb00f8ae6119787b665fd1196b816c1
Jonni <nyaganyamalima47@gmail.com>
2025-12-10 21:21:49 +0300

    cors setup

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,26 @@ import cors from "cors";
 
 const app = express();
 
-// const allowed = [process.env.FRONTEND_URL || "https://simple-e-commerce-website-nu.vercel.app" || "http://localhost:3000"];
-// app.use(cors({
-//   origin: function(origin, cb){
-//     if(!origin) return cb(null, true);
-//     if(allowed.indexOf(origin) !== -1) return cb(null, true);
-//     return cb(new Error('Not allowed by CORS'));
-//   }
-// }));
+console.log("Frontend URL:", process.env.FRONTEND_URL);
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+}));
+
 
 app.use(morgan("dev"));
 app.use(express.json());
