805265a5f10868f82c8acfa86acec76d17e0f30a
Jonni <nyaganyamalima47@gmail.com>
2025-12-10 14:36:38 +0300

    disabled cors

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,14 @@ import cors from "cors";
 
 const app = express();
 
-const allowed = [process.env.FRONTEND_URL || "https://simple-e-commerce-website-nu.vercel.app" || "http://localhost:3000"];
-app.use(cors({
-  origin: function(origin, cb){
-    if(!origin) return cb(null, true);
-    if(allowed.indexOf(origin) !== -1) return cb(null, true);
-    return cb(new Error('Not allowed by CORS'));
-  }
-}));
+// const allowed = [process.env.FRONTEND_URL || "https://simple-e-commerce-website-nu.vercel.app" || "http://localhost:3000"];
+// app.use(cors({
+//   origin: function(origin, cb){
+//     if(!origin) return cb(null, true);
+//     if(allowed.indexOf(origin) !== -1) return cb(null, true);
+//     return cb(new Error('Not allowed by CORS'));
+//   }
+// }));
 
 app.use(morgan("dev"));
 app.use(express.json());
